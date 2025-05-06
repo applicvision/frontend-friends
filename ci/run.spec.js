b1713d7c5cb1efebb6d562936cd44eaf1253d7dc
Måns Hommerberg <mans.hommerberg@applicvision.com>
2025-05-06 13:09:37 +0200

    Deep watch improvements, and improved testing.

--- ci/run.spec.js
+++ ci/run.spec.js
@@ -1,8 +1,79 @@
 import { test, expect } from '@playwright/test'
 
-test('Run the tests', async ({ page }) => {
+test('Check the unit tests', async ({ page }) => {
 	await page.goto('http://localhost:3000?expand');
 
 	await expect(page).toHaveTitle('All tests passed');
+
+})
+
+
+test('Test the todo app', async ({ page }) => {
+
+	await page.goto('http://localhost:3001/frontend-friends/examples/todo/');
+
+	await expect(page).toHaveTitle('ApplicVision - Frontend Friends');
+
+	await page.getByRole('textbox', { name: 'Enter new todo' }).fill('first todo')
+	await page.keyboard.press('Enter')
+
+	await expect(page.locator('#app').getByRole('listitem').locator('todo-box')).toBeEmpty()
+
+	await page.getByRole('textbox', { name: 'Enter new todo' }).fill('Another one')
+
+	await page.getByRole('button', { name: 'Add' }).click()
+
+	await expect(page.locator('#app').getByRole('listitem')).toHaveCount(2)
+
+	await expect(page.locator('#app')
+		.getByRole('listitem'))
+		.toContainText(['first todo', 'Another one'])
+
+	page.once('dialog', async dialog => {
+		await expect(dialog.defaultValue()).toBe('first todo')
+		await dialog.accept('Updated todo')
+	})
+
+	await page.locator('#app').getByRole('listitem').getByRole('button', { name: '✎' }).first().click()
+
+	await expect(page.locator('#app')
+		.getByRole('listitem'))
+		.toContainText(['Updated todo', 'Another one'])
+
+	await page.getByRole('listitem').filter({ hasText: 'Another one' }).locator('todo-box').click()
+	await expect(page.getByRole('listitem').filter({ hasText: 'Another one' }).locator('todo-box')).toHaveText('✓')
+
+	await page.reload()
+
+	await expect(page.locator('#app')
+		.getByRole('listitem'))
+		.toContainText(['Updated todo', 'Another one'])
+
+	await expect(
+		page.locator('#app').getByRole('listitem').filter({ hasText: 'Another one' }).locator('todo-box')
+	).toHaveText('✓')
+
+	await page.locator('#app').getByRole('radio', { name: 'Only done' }).check()
+
+	await expect(page.locator('#app').getByRole('listitem')).toHaveCount(1)
+	await expect(page.locator('#app').getByRole('listitem')).toContainText('Another one')
+
+	await page.locator('#app').getByRole('radio', { name: 'Only todo' }).check()
+
+	await expect(page.locator('#app').getByRole('listitem')).toHaveCount(1)
+	await expect(page.locator('#app').getByRole('listitem')).toContainText('Updated todo')
+
+	await page.locator('#app').getByRole('radio', { name: 'Show all' }).check()
+
+	await expect(page.locator('#app').getByRole('listitem')).toHaveCount(2)
+
+	await page.getByRole('button', { name: 'Clear done' }).click()
+
+	await expect(page.locator('#app').getByRole('listitem')).toHaveCount(1)
+	await expect(page.locator('#app').getByRole('listitem')).toContainText('Updated todo')
+
+	await page.locator('#app').getByRole('listitem').getByRole('button', { name: '✕' }).click()
+
+	await expect(page.locator('#app').getByRole('list')).toBeEmpty()
 
 })
--- playwright.config.js
+++ playwright.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
 	testDir: 'ci',
-	webServer: {
+	webServer: [{
 		command: 'npx test-browser',
 		url: 'http://localhost:3000',
 		reuseExistingServer: !process.env.CI
-	}
+	},
+	{
+		command: 'npm run serve-docs -- --no-watch --port 3001',
+		url: 'http://localhost:3001/frontend-friends/',
+		reuseExistingServer: !process.env.CI
+	}]
 })
